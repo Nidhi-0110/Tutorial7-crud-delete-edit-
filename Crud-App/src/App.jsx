6fa78691d856bab779ce3a6d2a3935bf0668f2cc
Nidhi-0110 <patelnidhi1628@gmail.com>
2025-03-17 09:36:30 +0530

    crud-delete-edit

---
+++ Crud-App/src/App.jsx
@@ -0,0 +1,20 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Table from './components/Table'
+import Form from './Components/Form'
+import { ToastContainer } from 'react-toastify'
+
+function App() {
+  return (
+    <div>
+        <BrowserRouter>
+           <Routes>
+              <Route path='/' element={<Table/>}/>
+              <Route path='/form' element={<Form/>}/>
+           </Routes>
+           <ToastContainer/>
+        </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
---
+++ Crud-App/src/server.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const API = "http://localhost:3001/users";
+async function getData() {
+  try {
+    const response = await axios.get(API);
+    return response.data
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function postData(obj) {
+  try {
+    await axios.post(API, obj);
+  } catch (error) {
+    console.log(error);
+  }
+}
+export { getData, postData };
